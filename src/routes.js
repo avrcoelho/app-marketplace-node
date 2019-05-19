48b4d35968334f4cf15d8cbaf9807ce0b000804e
Andre Coelho <avrcoelho@gmail.com>
2019-05-19 19:44:02 -0300

    add rota inicial

--- src/routes.js
+++ src/routes.js
@@ -11,20 +11,42 @@ const authMiddleware = require('./app/middlewares/auth')
 const controllers = require('./app/controllers')
 const validators = require('./app/validators')
 
-routes.post('/users', validate(validators.User), handle(controllers.UserController.store))
-routes.post('/sessions', validate(validators.Session), handle(controllers.SessionController.store))
+routes.post('/', (req, res) => res.send('Hello Wolrd'))
+routes.post(
+  '/users',
+  validate(validators.User),
+  handle(controllers.UserController.store)
+)
+
+routes.post(
+  '/sessions',
+  validate(validators.Session),
+  handle(controllers.SessionController.store)
+)
 
 // toda rota daqui para baixo vai precisar de  autenticação
 routes.use(authMiddleware)
 
 routes.get('/ads', handle(controllers.AdController.index))
 routes.get('/ads/:id', handle(controllers.AdController.show))
-routes.post('/ads', validate(validators.Ad), handle(controllers.AdController.store))
-routes.put('/ads/:id', validate(validators.Ad), handle(controllers.AdController.update))
+routes.post(
+  '/ads',
+  validate(validators.Ad),
+  handle(controllers.AdController.store)
+)
+routes.put(
+  '/ads/:id',
+  validate(validators.Ad),
+  handle(controllers.AdController.update)
+)
 routes.delete('/ads/:id', handle(controllers.AdController.destroy))
 
 // purchases
-routes.post('/purchases', validate(validators.Purchase), handle(controllers.PurchaseController.store))
+routes.post(
+  '/purchases',
+  validate(validators.Purchase),
+  handle(controllers.PurchaseController.store)
+)
 routes.get('/purchases', handle(controllers.PurchaseController.index))
 routes.put('/approvers/:id', handle(controllers.ApproverController.update))
 
